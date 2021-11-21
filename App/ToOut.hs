55422fad4284c5ce647274047c960975a642eb0d
Trouble-Truffle <perigordtruffle7318@gmail.com>
2021-11-21 19:17:49 +0800

    interactive mode

--- App/ToOut.hs
+++ App/ToOut.hs
@@ -1,8 +1,12 @@
 -- For unicode output
 module App.ToOut where
 
-import App.Parser (Element, SyntaxTree (Branch, Leaf))
-import Data.Char (chr, ord)
+import App.Args
+import App.Parser (Element, SyntaxTree (Branch, Leaf), foldParseFormula, formArray, parseFormula)
+import Control.Monad.Cont
+import Data.Char (chr, isUpper, ord)
+import Data.List
+import Text.Regex.PCRE
 
 toSubscript :: Int -> String
 toSubscript = map (chr . (+ 8272) . ord) . show
@@ -11,8 +15,31 @@ toLatexSub :: Int -> String
 toLatexSub x = "_{" ++ show x ++ "}"
 
 elemsToStr :: (Int -> String) -> [Element] -> String
-elemsToStr f = filter (/= '₁') . concatMap fromElem
+elemsToStr f = concatMap fromElem
   where
+    filterTrailingOne :: String -> String
+    filterTrailingOne a =
+      if a =~ "_1$"
+        then reverse $ drop 2 $ reverse a
+        else a
     fromElem :: Element -> String
-    fromElem (Leaf (al, ar)) = al ++ f ar
+    fromElem (Leaf (al, ar)) = al ++ (if ar == 1 then [] else f ar)
     fromElem (Branch (al, ar)) = "(" ++ concatMap fromElem al ++ ")" ++ f ar
+
+interactiveMode ::
+  ([Element] -> String) ->
+  (String -> Either String [Element]) ->
+  Either (IO ()) String
+interactiveMode f fs = Left $ interactLoop f fs
+
+interactLoop :: ([Element] -> String) -> (String -> Either String [Element]) -> IO ()
+interactLoop x y = do
+  putStrLn "Atom-Counter: Interactive mode"
+  withBreak $ \break ->
+    forM_ [1 ..] $ \i -> do
+      a <- liftIO getLine
+      when (a == ":q") $ break ()
+      liftIO . putStrLn $ either id x (y a)
+  liftIO . putStrLn $ "Interactive Mode End"
+  where
+    withBreak = flip runContT pure . callCC
--- Main.hs
+++ Main.hs
@@ -2,14 +2,16 @@ module Main where
 
 import App.Args (Options (optConsole))
 import qualified App.Args as A
+import App.Parser (applyNTimes)
 import qualified App.Parser as P
 import qualified App.ToOut as O
+import Data.Maybe (fromMaybe)
 import qualified Data.Version (showVersion)
 import qualified Paths_Atom_counter (version)
 import System.Environment (getArgs)
 import System.Exit
 
-lobby :: (A.Options, [String]) -> Either String String
+lobby :: (A.Options, [String]) -> Either (IO ()) String
 
 -- | do actions based on arguments
 lobby
@@ -28,18 +30,23 @@ lobby
       },
     xr
     )
+    | otherwise = Left $ print tableArgs
+    | sum (fromEnum <$> [showHaskCode, showLatex]) > 1 = Left $ putStrLn "Error: output method mixing"
     | showVersion = Right $ "Version: " ++ Data.Version.showVersion Paths_Atom_counter.version
     | showHaskCode = metaParse show
-    | showLatex = metaParse $ O.elemsToStr O.toLatexSub
+    | showLatex = metaParse $ (\a b -> ("(\\" ++) $ (++ "\\)") $ O.elemsToStr a b) O.toLatexSub
     | showUnicode = metaParse $ O.elemsToStr O.toSubscript
     where
-      metaParse :: ([P.Element] -> String) -> Either String String
-      metaParse f = unwords <$> mapM (fmap f . parseFormula' numOfFolds maxFold) xr
-        where
-          parseFormula' :: Int -> Bool -> (String -> Either String [P.Element])
-          parseFormula' x False = either Left (applyNtimes) P.parseFormula
+      metaParse :: ([P.Element] -> String) -> Either (IO ()) String
+      metaParse f
+        | isInteractive = O.interactiveMode f parseFormula'
+        | otherwise = either (Left . putStrLn) Right $ unwords <$> mapM (fmap f . parseFormula') xr
+      parseFormula' :: (String -> Either String [P.Element])
+      parseFormula'
+        | maxFold = P.foldParseFormula
+        | otherwise = flip P.parseFormula numOfFolds
 
 main :: IO ()
 main = do
   xIo <- A.compilerOpts <$> getArgs
-  xIo >>= (either putStrLn putStrLn . lobby)
+  xIo >>= (either id putStrLn . lobby)
